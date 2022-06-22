8317142f7a767f1a72d2cc7de8b296f4ee6745b5
João Vitor Wilke Silva <wilke.joao@gmail.com>
2022-06-21 19:59:46 -0700

    Added a header to the app

---
+++ my-app/src/components/Layout/Header.js
@@ -0,0 +1,20 @@
+import { Fragment } from "react";
+
+import mealsImage from "../../assets/meals.jpg";
+import classes from "./Header.module.css";
+
+const Header = (props) => {
+  return (
+    <Fragment>
+      <header className={classes.header}>
+        <h1>Brazilian Cantina</h1>
+        <button>Cart</button>
+      </header>
+      <div className={classes["main-image"]}>
+        <img src={mealsImage} alt="Meals image" />
+      </div>
+    </Fragment>
+  );
+};
+
+export default Header;
